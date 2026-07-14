dfd3f4ecf7985d14f1ccd051b6744a1d1a21b9f4
Kulthirat17 <14636@phanthong.ac.th>
2026-07-14 11:44:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll",function(){
 
+    const nav=document.querySelector("nav");
+
+    if(window.scrollY>50){
+
+        nav.style.background="#ffffff";
+
+    }else{
+
+        nav.style.background="#ffffff";
+
+    }
+
+});
