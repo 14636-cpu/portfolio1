30c776e104c0e4a448551e7bee8cac61da10f20c
Kulthirat17 <14636@phanthong.ac.th>
2026-07-16 14:46:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Welcome to My Portfolio");
+// เปลี่ยนสี Navbar เมื่อเลื่อนหน้า
+
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>80){
+
+nav.style.background="#0B3D2E";
+
+}else{
+
+nav.style.background="rgba(255,255,255,.08)";
+
+}
+
+});
